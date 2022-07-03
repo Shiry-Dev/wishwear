de3499b6ce56f0229dbfdad7503403a79dd5fd01
Gabriel Arrien <garrien1@gmail.com>
2022-07-03 13:42:36 -0400

    Criated Node Server

---
+++ backend/data.js
@@ -0,0 +1,54 @@
+const data = {
+    products: [
+        {
+            name: 'Camiseta Lula Livre',
+            slug: 'Camiseta Lula',
+            category: 'Camiseta',
+            image: '/images/7681b55e8e32f3acbd819e0657410b85.jfif', //679px x 829px
+            price: 120,
+            countInStock: 10,
+            brand: 'Lula',
+            rating: 4.5,
+            numReviews: 10, 
+            description: 'Camiseta Lula Livre para manifestações'
+        },
+        {
+            name: 'Camiseta Bolsonaro Presidente',
+            slug: 'Camiseta Bolsonaro',
+            category: 'Camiseta',
+            image: '/images/6e041179b8dfccbfb5c5be29454fe4bb.jfif',
+            price: 120,
+            countInStock: 10,
+            brand: 'Bolsonaro',
+            rating: 4.5,
+            numReviews: 10, 
+            description: 'Camiseta Bolsonaro Presidente para manifestações'
+        },
+        {
+            name: 'Camiseta Lula Livre',
+            slug: 'Camiseta Lula',
+            category: 'Camiseta',
+            image: '/images/7681b55e8e32f3acbd819e0657410b85.jfif',
+            price: 120,
+            countInStock: 10,
+            brand: 'Lula',
+            rating: 4.5,
+            numReviews: 10, 
+            description: 'Camiseta Lula Livre para manifestações'
+        },
+        {
+            name: 'Camiseta Lula Livre',
+            slug: 'Camiseta Lula',
+            category: 'Camiseta',
+            image: '/images/7681b55e8e32f3acbd819e0657410b85.jfif',
+            price: 120,
+            countInStock: 10,
+            brand: 'Lula',
+            rating: 4.5,
+            numReviews: 10, 
+            description: 'Camiseta Lula Livre para manifestações'
+        }
+    ],
+};
+
+export default data;
