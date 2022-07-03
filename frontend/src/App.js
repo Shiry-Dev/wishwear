fa5090577ec29e08c34e0035d9da7c720476e297
Gabriel Arrien <garrien1@gmail.com>
2022-07-03 12:50:47 -0400

    Imagens e reponsividade da home screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import data from "./data";
 
 
 function App() {
@@ -7,7 +8,24 @@ function App() {
         <a href="/">WishWear</a>
       </header>
       <main>
-        list products
+        <h1>Produtos em destaque</h1>
+        <div className="products">
+          {
+            data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name}/>
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p><strong>R${product.price}</strong></p>
+                <button>Add no carrinho</button>
+              </div>
+            </div>))
+          }
+        </div>
       </main>
     </div>
   );
