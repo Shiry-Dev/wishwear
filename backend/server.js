aa2599a7212df10ecf2cc080fe2601daa541e8c8
Gabriel Arrien <garrien1@gmail.com>
2022-07-04 22:28:46 -0400

    Signin API and Signin Screen

--- backend/server.js
+++ backend/server.js
@@ -4,27 +4,30 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGODB_URI).then(() => {
-  console.log('CONNECTED')
-}).catch(err => {
-  console.log(err.message);
-});
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('connected to db');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
 
-app.get('/api/products/:id', (req,res) => {
-  const product = data.products.find((x) => x._id === req.params.id);
-  if(product){
-    res.send(product);
-  }else{
-    res.status(400).send({messages: 'Produto não encontrado'});
-  }
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
 });
 
 const port = process.env.PORT || 5000;
