e3ae37f9ef64b0710539af887c809ff16d0b3bc4
Gabriel Arrien Fernandes <garrien1@gmail.com>
2022-07-08 21:32:31 -0400

    Edit product screen and update product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -10,6 +10,52 @@ productRouter.get('/', async (req, res) => {
   res.send(products);
 });
 
+productRouter.post(
+  '/',
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const newProduct = new Product({
+      name: 'Nome teste ' + Date.now(),
+      slug: 'Slug-teste' + Date.now(),
+      image: '/images/p1.jpg',
+      price: 0,
+      category: 'Cartegoria teste',
+      brand: 'Marca teste',
+      countInStock: 0,
+      rating: 0,
+      numReviews: 0,
+      description: 'Descrição teste',
+    });
+    const product = await newProduct.save();
+    res.send({ message: 'Produto criado', product });
+  })
+);
+
+productRouter.put(
+  '/:id',
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const productId = req.params.id;
+    const product = await Product.findById(productId);
+    if (product) {
+      product.name = req.body.name;
+      product.slug = req.body.slug;
+      product.price = req.body.price;
+      product.image = req.body.image;
+      product.category = req.body.category;
+      product.brand = req.body.brand;
+      product.countInStock = req.body.countInStock;
+      product.description = req.body.description;
+      await product.save();
+      res.send({ message: 'Produto atualizado' });
+    } else {
+      res.status(404).send({ message: 'Produto não encontrado' });
+    }
+  })
+);
+
 const PAGE_SIZE = 3;
 productRouter.get(
   '/admin',
